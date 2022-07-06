931dabb83d464303a152793618b0442aad4111cf
eduardo <eduardosantos17007@outlook.com>
2022-07-06 12:07:00 -0300

    Iniciando ultimos passos do projeto

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
 import Tmdb from "./Tmdb";
 import { MovieRow } from "./components/MovieRow";
-import {FeaturedMovie} from "./components/FeaturedMovie"
+import { FeaturedMovie } from "./components/FeaturedMovie";
+import { Header } from "./components/Header";
 import "./App.css";
 
 export const App = () => {
   const [movieList, setMovieList] = useState([]);
-  const [featuredData, setFeaturedData]= useState(null)
+  const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
 
   useEffect(() => {
     const loadAll = async () => {
@@ -15,20 +17,41 @@ export const App = () => {
       setMovieList(list);
 
       //pegando featured movie (apenas series netflix)
-      let originals = list.filter(i=> i.slug === 'originals');
-      let randomChoice = Math.floor(Math.random()*(originals[0].items.results.length - 1));
+      let originals = list.filter((i) => i.slug === "originals");
+      let randomChoice = Math.floor(
+        Math.random() * (originals[0].items.results.length - 1)
+      );
       let choice = originals[0].items.results[randomChoice];
-      let choiceInfo = await Tmdb.getMovieInfo(choice.id, 'tv');
-      
+      let choiceInfo = await Tmdb.getMovieInfo(choice.id, "tv");
+
       setFeaturedData(choiceInfo);
     };
     loadAll();
-  }, []); 
+  }, []);
+
+  useEffect(()=>{
+    
+    const scrollListener = () => {
+      if(window.scrollY > 100){
+        setBlackHeader(true);
+      }else{
+        setBlackHeader(false);
+      }
+    }
+
+    window.addEventListener('scroll', scrollListener);
+
+    return () =>{
+      window.removeEventListener('scroll', scrollListener)
+    }
+
+  },[]);
 
   return (
     <div className="page">
-     {featuredData &&
-     <FeaturedMovie item={featuredData}/>}
+      <Header black={blackHeader}/>
+
+      {featuredData && <FeaturedMovie item={featuredData} />}
       <section className="lists">
         {movieList.map((item, key) => (
           <MovieRow key={key} title={item.title} items={item.items} />
